2f4c558a070026013286c1a9d8dbce18954787de
JingChen <jingchenjc2019@gmail.com>
2019-04-04 17:50:05 -0500

    All functions works. Needs refactor.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,107 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetch(TRAINERS_URL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(trainerList){
+
+            for(let i = 0; i< trainerList.length; i++){
+                //get trainer name
+                const trainerName = trainerList[i].name
+
+                //create div for each card
+                let div = document.createElement('div')
+                div.setAttribute("class", "card")
+                div.setAttribute("data-id", `${trainerList[i].id}`)
+                //create div-p for each trainerName
+                let p = document.createElement('p')
+                div.appendChild(p)
+                p.innerText = trainerName
+                //create button for add pokemon
+                let addButton = document.createElement('button')
+                addButton.setAttribute("data-trainer-id", `${trainerList[i].id}`)
+                addButton.innerText = "Add Pokemon"
+                div.appendChild(addButton)
+                //add event to add Button
+
+                //create ul for each trainer
+                let ul = document.createElement('ul')
+                //get each trainer's pokemons
+                const pokemonList = trainerList[i].pokemons
+                //iterate pokemon list
+                for(let j = 0; j < pokemonList.length; j++){
+                    //create li for each pokemon
+                    let li = document.createElement('li')
+                    //release button
+                    let releaseButton = document.createElement('button')
+                    releaseButton.setAttribute("class", "release")
+                    releaseButton.setAttribute("data-pokemon-id", `${pokemonList[j].id}`)
+                    releaseButton.innerText = "Release"
+                    //add event to releaseButton
+                    releaseButton.addEventListener('click', function(){
+                        fetch(`${POKEMONS_URL}/${pokemonList[j].id}`, {
+                            method: "DELETE"
+                        })
+                        li.remove();
+                    })
+                    //add li innerText
+                    li.innerHTML = `${pokemonList[j].nickname} (${pokemonList[j].species})`
+                    li.append(releaseButton)
+                    //add li to ul
+                    ul.append(li)
+                }
+                div.append(ul)
+                main.append(div)
+
+                //fetch pokemon data
+                if (pokemonList.length < 6){
+                    fetch(`${POKEMONS_URL}`, {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            "trainer_id": trainerList[i].id
+                        })
+                    })
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (pokemonData) {
+                        //add event to addButton
+                        addButton.addEventListener('click', function (e) {
+                        e.preventDefault();
+                        {
+                        let li = document.createElement('li')
+                        li.innerText = `${pokemonData.nickname}`
+                        ul.append(li)
+                        
+                                //release button
+                        let releaseButton = document.createElement('button')
+                        releaseButton.setAttribute("class", "release")
+                                releaseButton.setAttribute("data-pokemon-id", `${pokemonData.id}`)
+                        releaseButton.innerText = "Release"
+                                //add event to releaseButton
+                        releaseButton.addEventListener('click', function () {
+                            fetch(`${POKEMONS_URL}/${pokemonData.id}`, {
+                                method: "DELETE"
+                            })
+                               li.remove();
+                            })
+                        
+                        //append release Button
+                        li.append(releaseButton)
+                        }
+
+                        })
+                    })
+                }
+            }       
+        })   
+    
+})
